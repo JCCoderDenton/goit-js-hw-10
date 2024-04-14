8cd53191a73a7374319148969210fd8b87dd5a67
JCCoderDenton <denys_m99@ukr.net>
2024-04-14 03:04:07 +0300

    3

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const { selector, divCatInfo, loader, error } = ref;
 loader.classList.replace('loader', 'is-hidden');
 error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
-
 let arrBreedsId = [];
 fetchBreeds()
 .then(data => {
@@ -32,21 +31,25 @@ fetchBreeds()
 selector.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
+    const check = divCatInfo.innerHTML.slice(0, 21);
+    if (check == `<div class="box-img">`)
     loader.classList.replace('is-hidden', 'loader');
     selector.classList.add('is-hidden');
     divCatInfo.classList.add('is-hidden');
-
     const breedId = event.currentTarget.value;
     fetchCatByBreed(breedId)
     .then(data => {
         loader.classList.replace('loader', 'is-hidden');
         selector.classList.remove('is-hidden');
         const { url, breeds } = data[0];
-        
-        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
+        const check = divCatInfo.innerHTML.slice(0, 21);
+        if (check == `<div class="box-img">`)
         divCatInfo.classList.remove('is-hidden');
+        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
     })
+    
     .catch(onFetchError);
+
 };
 
 function onFetchError(error) {
